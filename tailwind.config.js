f63a2aa26993c66b2194b0374e71a90d2d6a9e39
Acaz-GD <alessandroazevedoplay@gmail.com>
2023-04-04 17:42:37 -0300

    scroll style ajust

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-
       fontFamily: {
         // burtons: "burtons",
         poppins: ["Poppins", "sans-serif"],
@@ -16,12 +15,43 @@ module.exports = {
       colors: {
         bg_all: '#1F2945',
       }, 
+      
 
+      scrollbar: theme => ({
+        DEFAULT: {
+          size: theme('spacing.2'),
+          track: {
+            background: theme('colors.bg_all'),
+          },
+          thumb: {
+            background: theme('colors.red.500'),
+            borderRadius: '10px',
+          },
+          hover: {
+            background: theme('colors.amber.500'),
+          },
+        },
+      }),
     },
   },
 
   plugins: [
-    require('tailwind-scrollbar')
+    require ('@gradin/tailwindcss-scrollbar')
+    // ({
+    //   size: '10px',
+    //   track: {
+    //     background: '#1F2945',
+    //   },
+    //   thumb: {
+    //     background: 'red',
+    //     borderRadius: '10px',
+        
+    //   },
+    //   hover: {
+    //     background: 'darkgray',
+    //     borderRadius: '10px',
+    //   },
+    // }),  
   ],
 
   variants: {
