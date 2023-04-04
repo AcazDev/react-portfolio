b139faf82f15fad9902c383fedf93e578cdf4749
Acaz-GD <alessandroazevedoplay@gmail.com>
2023-04-04 01:34:37 -0300

    projeto portfolio  inicial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,html,js}",
+    "./components/**/*.{js,ts,jsx,tsx,html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // burtons: "burtons",
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        bg_all: '#1F2945',
+        
+      }
+    },
+  },
+  plugins: [],
+};
